a34b606c51e2b0520572ae71803ad983dcfbbe24
chost2004 <59009916+chost2004@users.noreply.github.com>
2019-12-23 23:59:41 +0200

    DZ2 task4

---
+++ dz2/task4-2.py
@@ -0,0 +1,25 @@
+
+W = int(input('цифру от 0 до 9: '))
+if W == 0:
+    print (')')
+elif W == 1:
+    print ('!')
+elif W == 2:
+    print ('@')
+elif W == 3:
+    print ('#')
+elif W == 4:
+    print ('$')
+elif W == 5:
+    print ('%')
+elif W == 6:
+    print ('^')
+elif W == 7:
+    print ('&')
+elif W == 8:
+    print ('*')
+elif W == 9:
+    print ('(')
+else:
+    print('ошибка')
+input ('Нажмите Enter для выхода')
