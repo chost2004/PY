c344d257085d49b83955eb34aa8ae879d7e352c6
chost2004 <59009916+chost2004@users.noreply.github.com>
2020-01-10 12:08:42 +0200

    dz5 task4.1

--- dz5/task3.py
+++ dz5/task3.py
@@ -1,7 +1,8 @@
 string = 'Yesterday, we bumped into Laura. It had to happen, but you can\'t deny the timing couldn\'t be worse. The "mission" to try and seduce her was a complete failure last month. By the way, she still has the ring I gave her. Anyhow, it hasn\'t been a pleasurable experience to go through it. I wanted to feel done with it first.'
-key = string.split('. ')[0]
+#key = string.split('. ')[0]
 text = string.split('. ')
-key = key.split()
+#key = key.split()
+key = text[0].split()
 for i in range (len(key)):
     key[i] = key[i].strip(',').strip('"')
     
