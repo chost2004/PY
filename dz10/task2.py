e5611cf2717397f01fd1b40ae9c03a0dc87b2cb1
chost2004 <59009916+chost2004@users.noreply.github.com>
2020-01-28 03:41:44 +0200

    dz10

--- dz10/task2.py
+++ dz10/task2.py
@@ -9,4 +9,23 @@
 результат работы метода getCode()
 '''
 class Employee:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+rab1 = Employee('Petrov', 50)
+rab2 = Employee('Ivanov', 45)
+rab3 = Employee('Sidorov', 20)
+rabs = [rab1, rab2, rab3]
+
+class EmpTable:
+    def __init__(self, rabs):
+        self.rabs = rabs
+    def getCode(self):
+        for rab in rabs:
+            print (rab.name, rab.age)
+
+tabl = EmpTable(rabs)
+tabl.getCode()
+    
     
